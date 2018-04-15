3a19f23e79679f73da7b6f6125ee814e672aa2ac
Yoshitaka Terazawa <locol6622@gmail.com>
2018-04-15 14:01:57 +0900

    update: Add styled-components

--- src/scripts/Top.js
+++ src/scripts/Top.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { pure } from 'recompose';
+import styled from 'styled-components';
 
 const Top = () => {
   return (
-    <div>
+    <Layout>
       <h1>Hello React</h1>
-    </div>
+    </Layout>
   );
 };
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 export default pure(Top);
